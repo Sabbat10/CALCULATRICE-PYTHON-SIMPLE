d98f26f02a2decfccc89e95bbf538a739d7bf12c
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-05-28 09:49:14 +0100

    add file calculator

---
+++ index.py
@@ -0,0 +1,25 @@
+## les variables
+nombre_un = 2
+nombre_deux = 0
+
+## les fonctions
+def addition(premier_nombre, deuxieme_nombre):
+    return premier_nombre + deuxieme_nombre
+
+def multiplication(premier_nombre, deuxieme_nombre):
+    return premier_nombre * deuxieme_nombre
+
+def soustraction(premier_nombre, deuxieme_nombre):
+    return premier_nombre - deuxieme_nombre
+
+def division(premier_nombre, deuxieme_nombre):
+    if nombre_deux == 0:
+        return "Impossible de diviser par 0"
+    return premier_nombre / deuxieme_nombre
+
+## les instructions
+print(f"{nombre_un} + {nombre_deux} = {addition(nombre_un, nombre_deux)}")
+print(f"{nombre_un} * {nombre_deux} = {multiplication(nombre_un, nombre_deux)}")
+print(f"{nombre_un} - {nombre_deux} = {soustraction(nombre_un, nombre_deux)}")
+print(f"{nombre_un} / {nombre_deux} = {division(nombre_un, nombre_deux)}")
+
