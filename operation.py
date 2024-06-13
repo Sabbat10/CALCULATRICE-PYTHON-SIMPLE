4b7e0b4e69648cc8690976bad7e8285e13b124cc
sabbat10 <sabbatlumpatshia99@gmail.com>
2024-06-13 11:22:23 +0100

    add try except ton the divide operation

--- operation.py
+++ operation.py
@@ -11,8 +11,9 @@ def soustraction(premier_nombre, deuxieme_nombre):
     return resultat
 
 def division(premier_nombre, deuxieme_nombre):
-    if deuxieme_nombre == 0:
-        return("Vous ne pouvez pas diviser par 0")
-    resultat = premier_nombre / deuxieme_nombre
-    return resultat
+    try:
+        resultat = premier_nombre / deuxieme_nombre
+        return int(resultat)
+    except ZeroDivisionError:
+        return("Impossible de diviser par 0")
 
